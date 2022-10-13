4df6a98d10413ef858db6ec9981ecb5555745ad4
Samuel Koob <56673835+sam-k0@users.noreply.github.com>
2022-10-13 22:58:26 +0900

    Done I think LOL

--- Assignment01/Assignment01.cpp
+++ Assignment01/Assignment01.cpp
@@ -36,27 +36,73 @@ int getInputCleaned()
 	}
 }
 
-double findLowest(int* arr, int n) // finds the lowest number in array
+double findLowest(double* numbers, int n) // finds the lowest number in array
 {
-	int i;
-	double lowest;
-	lowest = 11;
-	double current;
-	printf("Find lowest\n");
+	int i; // for the forloop
+	double lowest; // Stores the currently lowest value
+	lowest = 11;   // only numbers from 1 to 10 exist, so 11 is out of bounds
+	double current; // Holds the current number
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < (int)n; i++) // Loop all numbers
 	{
-		current = arr[i];
+		current = numbers[i]; // get the current
 
-		printf("curr: %d", current);
-
-		if (current < lowest)
+		if (current < lowest) // compare current number to lowest and assign if true
 		{
 			lowest = current;
 		}
 	}
-
 	return lowest;
+}
+
+
+double findHighest(double* numbers, int n) // finds the lowest number in array
+{
+	int i; // for the forloop
+	double highest; // Stores the currently lowest value
+	highest = 0;   // only numbers from 1 to 10 exist, so 0 is always lowest
+	double current; // Holds the current number
+
+	for (i = 0; i < (int)n; i++) // Loop all numbers
+	{
+		current = numbers[i]; // get the current
+
+		if (current > highest) // compare current number to lowest and assign if true
+		{
+			highest = current;
+		}
+	}
+	return highest;
+}
+
+void calcScore(double* numbers, int n)
+{
+	int lowest, highest, total;
+	int i;
+	double current, average;
+	total = 0;
+
+	lowest = findLowest(numbers, n);
+	highest = findHighest(numbers, n);
+
+	for (i = 0; i < (int)n; i++) // Loop all numbers
+	{
+		current = numbers[i]; // get the current
+
+		total += current; // Add up to total
+	}
+
+	total -= highest + lowest; // subtract the lowest and highest
+
+	if (total <= 0 || n - 2 <= 0) // if there are only 2 numbers, we would divide by zero
+	{
+		printf("Not enough numbers in array...");
+		return;
+	}
+
+	average = (double)total / (n - 2);
+
+	printf("Average: %f\n", average);
 }
 
 int main()
@@ -68,33 +114,32 @@ int main()
 	*/
 	int num;
 	int maxAmount;
-	int* numArr;
+	double* numArr;
 	// Set the limit for numbers (maxAmount)
 	maxAmount = (int)getInputCleaned();
 
 	printf("Max Amount is: %d \n", maxAmount);
-
-	numArr = (int*)calloc(maxAmount, sizeof(int));
 	
-
+	// Dynamic array allocation
+	// https://www.scaler.com/topics/c/dynamic-array-in-c/
+	numArr = (double*)calloc(maxAmount, sizeof(double));
+	
 	// Loop maxAmount times
 	int i;
 	for (i = 0; i < maxAmount; i++)
 	{
-		printf("%d number: \n", i+1);
+		//printf("%d number: \n", i+1);
 		num = getInputCleaned();
-		numArr[i] = num;
+		numArr[i] = double(num);
 	}
 
-	// Do stuff with array
-	for (i = 0; i < maxAmount; i++)
-	{
-		printf("%d number: %d\n", i, numArr[i]);
-	}
+	// Do stuff with the numbers
 
-	printf("-----------------------------");
+	printf("-----------------------------\n");
+	printf("Lowest: %d", (int)findLowest(numArr, maxAmount));
+	printf("Highest: %d", (int)findHighest(numArr, maxAmount));
 
-	printf("Lowest: %d", findLowest(numArr, maxAmount));
+	calcScore(numArr, maxAmount);
 
 	return 0;
 }
